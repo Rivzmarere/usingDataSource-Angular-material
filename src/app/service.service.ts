54ee8267171d225e053856c8bfa9b69f075c9f15
rivaldomarere <rivaldom@afrosoft.co.zw>
2022-02-08 12:06:48 +0200

    services now set

--- src/app/service.service.ts
+++ src/app/service.service.ts
@@ -1,9 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpResponse, HttpErrorResponse } from '@angular/common/http';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServiceService {
 
-  constructor() { }
+  constructor(private http: HttpClient,) { }
+
+  getAllProducts() {
+    return this.http.get(
+      `https://jsonplaceholder.typicode.com/posts`);}
+
 }
--- src/app/table/table.component.ts
+++ src/app/table/table.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ServiceService } from '../service.service';
 
 @Component({
   selector: 'app-table',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TableComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: ServiceService) { }
 
   ngOnInit(): void {
+    this.service.getAllProducts().subscribe(res =>{
+      console.log(res)
+    })
   }
 
 }
